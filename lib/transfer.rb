68d20e4a7549dbcaf1820e7c041711ccbfe21777
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-10 05:12:22 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, status="pending", amount)
+    @sender = sender 
+    @receiver = receiver 
+    @status = status 
+    @amount = amount
+  end 
+  
+  def valid? 
+    self.sender.valid? 
+    self.receiver.valid?
+  end 
+  
+  def execute_transaction 
+    self.sender.balance -= self.amount 
+    self.receiver.balance += self.amount
+  end 
 end
