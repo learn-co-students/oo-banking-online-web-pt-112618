283532130e7de8e8c09d296dae91b04bded5bbbb
norawolf <noraevogt@gmail.com>
2019-01-11 14:33:01 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :pending, :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    # return from method if it has already been called. is there a better way?
+    return if self.status == "complete"
+
+    sender.balance -= amount
+    receiver.balance += amount
+    self.status = "complete"
+
+    #calls helper method with rejected message if valid? method evaluates to false
+    rejected if !valid?
+  end
+
+  def rejected
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
+  end
 end
